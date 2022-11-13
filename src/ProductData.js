29012d6f1c996deb6320a7da5412e0c2e41059eb
Zamir Lalji <zamirlal15@gmail.com>
2022-11-13 12:59:03 -0500

    New Project

---
+++ src/ProductData.js
@@ -0,0 +1,29 @@
+const ProductData = {
+    title: 'Watch 19 - The Smartest Watch',
+    description: 'The Most smartest watch out there. Buy it Now',
+    colorOptions: [
+        {
+            styleName: 'Black Strap',
+            imageUrl: 'https://imgur.com/iOeUBV7.png'
+        },
+        {
+            styleName: 'Red Strap',
+            imageUrl: 'https://imgur.com/PTgQlim.png'
+        },
+        {
+            styleName: 'Blue Strap',
+            imageUrl: 'https://imgur.com/Mplj1YR.png'
+        },
+        {
+            styleName: 'Purple Strap',
+            imageUrl: 'https://imgur.com/xSIK4M8.png'
+        },
+    ],
+    featureList: [
+        "Time", "Heart Rate"
+    ]
+}
+
+Object.freeze(ProductData); //This line of code just makes your object as a constant. No values can be updated.
+
+export default ProductData;
---
+++ src/ProductPreview.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ProductPreview = (props) => {
+  return (
+    <div>
+      <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+
+      {/*<div className={classes.TimeSection}>
+            <p>{`${currentHour}:${currentMinute}`}</p>
+            </div>
+            </div>*/}
+
+      <div className={classes.HeartBeatSection}>
+        <i class="fas fa-heartbeat"></i>
+        <p>78</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPreview;
