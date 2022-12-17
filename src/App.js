2104589de35fb78dcd4a41eb79e195d2a8b82661
Zamir Lalji <zamirlal15@gmail.com>
2022-12-17 16:19:30 -0500

    Added New Files and Removed files

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 
 import classes from './App.module.css';
-import ProductData from './ProductData';
+import ProductPreview from './ProductPreview/ProductPreview';
+import ProductDetails from './ProductDetails/ProductDetails';
+import Topbar from './Topbar/Topbar.module.css';
+import ProductData from './Utils/ProductData';
 
-function App() {
-  const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
-  const currentMinute = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <nav className={classes.Topbar}>
-          <img src="https://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo" />
-        </nav>
-      </header>
+class App extends Component {
+	state = {
+		productData: ProductData,
+		// currentPreviewImage: 'https://imgur.com/xSIK4M8.png',
+		currentPreviewImagePos: 0,
+		currentSelectedFeature: 0,
+	}
 
-      <div className={classes.MainContainer}>
-        <div className={classes.ProductPreview}>
-          <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+	onColorOptionClick = (pos) => {
+		// const updatedPreviewImage = this.state.productData.colorOptions[pos].imageUrl
+		this.setState({currentPreviewImagePos: pos});
+	}
 
-          {/*<div className={classes.TimeSection}>
-            <p>{`${currentHour}:${currentMinute}`}</p>
-          </div>
-          </div>*/}
-          
-          <div className={classes.HeartBeatSection}>
-            <i class="fas fa-heartbeat"></i>
-            <p>78</p>
-          </div>
-          </div>
+	onFeatureItemClick = (pos) => {
+		this.setState({currentSelectedFeature: pos});
+	}
 
-        <div className={classes.ProductData}>
-          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-          <p className={classes.ProductDescription}>{ProductData.description}</p>
+	render() {
+		return (
+			<div className="App">
+			   <Topbar />
 
-          <h3 className={classes.SectionHeading}>Select Color</h3>
-          <div>
-            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/PTgQlim.png" alt="Red Colored Watch" /> 
-            <img className={classes.ProductImage} src="https://imgur.com/iOeUBV7.png" alt="Black Colored Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Colored Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Colored Watch" />
-          </div>
+			   <div className={classes.MainContainer}>
+			      	<div className={classes.ProductPreview}>
+			      		<ProductPreview currentPreviewImage={this.state.productData.colorOptions
+			      		[this.state.currentPreviewImagePos].imageUrl} currentSelectedFeature=
+			      		{this.state.currentSelectedFeature} />
+			      	</div>
 
-          <h3 className={classes.SectionHeading}>Features</h3>
-          <div>
-            <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
-            <button className={classes.FeatureItem}>Heart Rate</button>
-          </div>
-
-          <button className={classes.PrimaryButton}>Buy Now</button>
-        </div>
-      </div>
-    </div>
-  );
+			      	<div className={classes.ProductData}>
+			      		<ProductDetails data={this.state.productData} onColorOptionClick=
+			      		{this.onColorOptionClick} currentPreviewImagePos=
+			      		{this.state.currentPreviewImagePos} onFeatureItemClick=
+			      		{this.onFeatureItemClick} currentSelectedFeature=
+			      		{this.state.currentSelectedFeature} />
+			      	</div>
+			   </div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Topbar/Topbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Tobbar.module.css';
+
+const Topbar = () => {
+	return(
+		<header>
+			<nav className={classes.Topbar}>
+			<img src="https://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo" />
+			</nav>
+		</header>
+	)
+}
+
+export default Topbar;
